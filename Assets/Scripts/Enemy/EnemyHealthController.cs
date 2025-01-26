7908786af60071c3e77d879d5e49eb063fc8b4d4
Armen <armenkl2001@gmail.com>
2025-01-26 15:41:19 +0400

    - fire effect

--- Assets/Scripts/Enemy/EnemyHealthController.cs
+++ Assets/Scripts/Enemy/EnemyHealthController.cs
@@ -7,9 +7,17 @@ public class EnemyHealthController : MonoBehaviour
     [SerializeField] private EnemyAnimationController _enemyAnimationController;
     [SerializeField] private EnemyAttackController _enemyAttackController;
     [SerializeField] private NavMeshAgent _navMeshAgent;
-    
+
+    [SerializeField] private SkinnedMeshRenderer _skinnedMeshRenderer;
+    [SerializeField] private Material _fireMaterial;
+    [SerializeField] private Material _unfireMaterial;
+
     private int _health = 100;
     private bool _isDead = false;
+
+    private Tween _tween;
+    private Tween _duration;
+    private bool _isBurning;
 
     public void GetDamage(int damage)
     {
@@ -37,5 +45,29 @@ public class EnemyHealthController : MonoBehaviour
     public void SetHealth(int health)
     {
         _health = health;
+    }
+
+    public void FireEffect()
+    {
+        if (!_isBurning)
+        {
+            _isBurning = true;
+
+            Sequence sequence = DOTween.Sequence();
+            sequence.AppendCallback(() => _skinnedMeshRenderer.material = _fireMaterial)
+                    .AppendInterval(0.25f)
+                    .AppendCallback(() => _skinnedMeshRenderer.material = _unfireMaterial)
+                    .AppendInterval(0.25f)
+                    .SetLoops(-1, LoopType.Restart);
+            _tween = sequence;
+        }
+
+        _duration.Kill();
+        _duration = DOVirtual.DelayedCall(5, () =>
+        {
+            _tween.Kill();
+            _skinnedMeshRenderer.material = _unfireMaterial;
+            _isBurning = false;
+        });
     }
 }
