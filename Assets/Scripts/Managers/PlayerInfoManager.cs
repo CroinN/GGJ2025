7f5c33b18685b1fc9661930e4351acbeb662156f
Ashot Gasparyan <ashotgasparyan@Ashots-MacBook-Pro.local>
2025-01-26 11:26:47 +0400

    - healthbar

--- Assets/Scripts/Managers/PlayerInfoManager.cs
+++ Assets/Scripts/Managers/PlayerInfoManager.cs
@@ -1,12 +1,18 @@
-using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerInfoManager : MonoBehaviour, IService
 {
+   [SerializeField] private Slider _healthbar;
    [SerializeField] private Transform _playerTransform;
    
    public Transform PlayerTransform{ get => _playerTransform; set => _playerTransform = value; }
-
+   
+   public void UpdateHealth(float healthFactor)
+   {
+      _healthbar.value = healthFactor;
+   }
+   
    private void Awake()
    {
       RegisterService();
--- Assets/Scripts/Player/PlayerHealthContoller.cs
+++ Assets/Scripts/Player/PlayerHealthContoller.cs
@@ -1,14 +1,22 @@
+using System;
 using UnityEngine;
 
 public class PlayerHealthContoller : MonoBehaviour
 {
     [SerializeField] private int _maxHealth = 150;
     
+    private PlayerInfoManager _playerInfoManager;
     private int _health;
 
     private void Awake()
     {
         _health = _maxHealth;   
+    }
+
+    private void Start()
+    {
+        _playerInfoManager = SL.Get<PlayerInfoManager>();
+        _playerInfoManager.UpdateHealth(1);
     }
 
     public void GetDamage(int damage)
@@ -20,6 +28,7 @@ public class PlayerHealthContoller : MonoBehaviour
         {
             OnDie();
         }
+        _playerInfoManager.UpdateHealth((float)_health/(float)_maxHealth);
     }
 
     private void OnDie()
